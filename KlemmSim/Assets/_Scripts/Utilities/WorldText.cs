db5a5a9bba29caa20fc73d5828ab908661912559
DerHyper <maxrichter15@web.de>
2024-04-14 19:54:14 +0200

    Add WorldText Utility

---
+++ KlemmSim/Assets/_Scripts/Utilities/WorldText.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+// Source: https://youtu.be/waEsGu--9P8?si=GNZfMSbodwKZ8iKo&t=336
+class WorldText
+{
+    // Create Text in World
+    public static TextMesh CreateWorldText(
+        string text,
+        Vector3 localPosition,
+        int fontSize,
+        Color color
+        ) 
+    {
+        GameObject gameObject = new GameObject("TextObject");
+        gameObject.AddComponent<TextMesh>();
+        Transform transform = gameObject.transform;
+        transform.Rotate(90,0,0);
+        transform.localPosition = localPosition;
+
+        TextMesh textMesh = gameObject.GetComponent<TextMesh>();
+        textMesh.text = text;
+        textMesh.fontSize = fontSize;
+        textMesh.color = color;
+        textMesh.anchor = TextAnchor.MiddleCenter;
+        textMesh.alignment = TextAlignment.Center;
+        return textMesh;
+    }
+}
