ec8b1b72c3de324e69bb22c5d1ffe20cdcbf6279
DerHyper <maxrichter15@web.de>
2024-04-18 18:38:22 +0200

    Add CameraToWorld class for grid mouse position

---
+++ KlemmSim/Assets/_Scripts/Utilities/CameraToWorld.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class CameraToWorld
+{
+    private static float distanceMultiplier = 0.1f;
+
+    // Calculate the position of the block adjacent to the face of the block the mouse is pointing towards
+    public static Vector3Int GetMouseBlockPlacementPosition()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Physics.Raycast(ray, out RaycastHit raycastHit);
+        Vector3 positionAdjacentToBlock = raycastHit.point + distanceMultiplier*raycastHit.normal;
+        Vector3Int flooredPosition = Vector3Int.FloorToInt(positionAdjacentToBlock);
+        return flooredPosition;
+    }
+
+    public static Vector3 GetMousePosition()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Physics.Raycast(ray, out RaycastHit raycastHit);
+        Vector3 raycastHitPoint = raycastHit.point;
+        return raycastHitPoint;
+    }
+}
