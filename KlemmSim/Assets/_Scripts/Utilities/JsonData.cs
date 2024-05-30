2c308efcafca2ad90926bd347c650603876619eb
DerHyper <maxrichter15@web.de>
2024-05-30 13:23:27 +0200

    Add testing environment

--- KlemmSim/Assets/_Scripts/Utilities/JsonData.cs
+++ KlemmSim/Assets/_Scripts/Utilities/JsonData.cs
@@ -8,30 +8,30 @@ using System.Linq;
 [Serializable]
 public class JsonData
 {
-    public List<JsonDataBlockInfo> jsonDataBlockInfos;
+    public List<RelevantBlockData> jsonDataBlockInfos;
 
     // Constructor creates a List entry for every BuildingBlockDisplay in the given Array.
     // Each entry contains data, relevant for saving and loading blocks into the scene
     public JsonData(BuildingBlockDisplay[] blocks)
     {
-        jsonDataBlockInfos = new List<JsonDataBlockInfo>();
+        jsonDataBlockInfos = new List<RelevantBlockData>();
         foreach (BuildingBlockDisplay block in blocks)
         {
-            JsonDataBlockInfo jsonDataBlockInfo = new JsonDataBlockInfo(block.name, block.position);
+            RelevantBlockData jsonDataBlockInfo = new RelevantBlockData(block.name, block.position);
             jsonDataBlockInfos.Add(jsonDataBlockInfo);
         }
     }
 
     // relevant data for saving and loading blocks into the scene
     [Serializable]
-    public class JsonDataBlockInfo
+    public class RelevantBlockData
     {
         [SerializeField]
         public string name;
         [SerializeField]
         public Vector3Int position;
 
-        public JsonDataBlockInfo(string name, Vector3Int position)
+        public RelevantBlockData(string name, Vector3Int position)
         {
             this.name = name;
             this.position = position;
