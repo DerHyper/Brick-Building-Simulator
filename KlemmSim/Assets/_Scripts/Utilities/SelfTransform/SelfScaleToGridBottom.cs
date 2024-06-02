430c5cf988bf6b81455113bfb4021cbda4834ea2
DerHyper <maxrichter15@web.de>
2024-06-02 14:26:49 +0200

    Refactor SelfScaleToGridBottom

--- KlemmSim/Assets/_Scripts/Utilities/SelfTransform/SelfScaleToGridBottom.cs
+++ KlemmSim/Assets/_Scripts/Utilities/SelfTransform/SelfScaleToGridBottom.cs
@@ -1,13 +1,22 @@
+using System.Threading.Tasks;
 using UnityEngine;
 
 public class SelfScaleToGridBottom : MonoBehaviour
 {
-    public float scalingFactor;
+    [SerializeField]
+    private float scalingFactor = 0.1f; // Dependent on the dimensions of the object this component is used on (cube:1, plane:0.1, ...)
+    [SerializeField]
+    private float yScale = 1;
     void Start()
     {
-        Vector3Int gridSize = Finder.FindOrCreateObjectOfType<GridManager>().GetSize();
-        Vector3 scale = new Vector3(gridSize.x, 1, gridSize.z)*scalingFactor;
-        gameObject.transform.localScale = scale;
+        GridManager gridManager = Finder.FindOrCreateObjectOfType<GridManager>();
+        UpdateScale(gridManager, scalingFactor, yScale);
     }
 
+    public void UpdateScale(GridManager gridManager, float scalingFactor, float yScale)
+    {
+        Vector3Int gridSize = gridManager.GetSize();
+        Vector3 scale = new Vector3(gridSize.x, 0, gridSize.z) * scalingFactor + new Vector3(0, yScale, 0);
+        gameObject.transform.localScale = scale;
+    }
 }
