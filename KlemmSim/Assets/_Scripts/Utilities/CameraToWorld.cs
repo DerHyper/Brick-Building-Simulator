2d1c09dbf8e086d5caf67f86034a4b4d49a22655
DerHyper <maxrichter15@web.de>
2024-06-20 13:42:36 +0200

    Refactor CameraToWorld
    Extracted double Methods

--- KlemmSim/Assets/_Scripts/Utilities/CameraToWorld.cs
+++ KlemmSim/Assets/_Scripts/Utilities/CameraToWorld.cs
@@ -2,15 +2,13 @@ using UnityEngine;
 
 public static class CameraToWorld
 {
-    private static float s_distanceMultiplier = 0.1f;
+    private const float s_distanceMultiplier = 0.1f;
 
     // Calculate the position of the block adjacent to the face of the block the mouse is pointing towards
     public static Vector3Int GetMouseBlockPlacementPosition()
     {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        Physics.Raycast(ray, out RaycastHit raycastHit);
-        if (raycastHit.collider == null) throw new OutsideGridException();
-        Vector3 positionAdjacentToBlock = raycastHit.point + s_distanceMultiplier*raycastHit.normal;
+        RaycastHit mouseHit = GetMouseRaycastHit();
+        Vector3 positionAdjacentToBlock = mouseHit.point + s_distanceMultiplier * mouseHit.normal;
         Vector3Int flooredPosition = Vector3Int.FloorToInt(positionAdjacentToBlock);
         return flooredPosition;
     }
@@ -25,10 +23,16 @@ public static class CameraToWorld
 
     public static GameObject GetGameObjectAtMousePosition()
     {
+        RaycastHit mouseHit = GetMouseRaycastHit();
+        GameObject raycastHitGameObject = mouseHit.collider.gameObject;
+        return raycastHitGameObject;
+    }
+
+    private static RaycastHit GetMouseRaycastHit()
+    {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         Physics.Raycast(ray, out RaycastHit raycastHit);
         if (raycastHit.collider == null) throw new OutsideGridException();
-        GameObject raycastHitGameObject = raycastHit.collider.gameObject;
-        return raycastHitGameObject;
+        return raycastHit;
     }
 }
