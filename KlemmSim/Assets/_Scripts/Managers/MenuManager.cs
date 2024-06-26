ccc63863450f11c6755442d81d77ec828ff443bc
DerHyper <maxrichter15@web.de>
2024-06-26 13:16:11 +0200

    Add MenuManager

---
+++ KlemmSim/Assets/_Scripts/Managers/MenuManager.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    public GameObject ExitCanvas;
+    public GameObject ExitButton;
+    public SaveManager SaveManager;
+    void Start()
+    {
+        InitiateExitMenu();
+    }
+
+    private void InitiateExitMenu()
+    {
+        #if UNITY_WEBGL && !UNITY_EDITOR
+            ExitButton.SetActive(false); // No exit on WebGL
+        #endif
+            ExitCanvas.SetActive(false);
+    }
+
+    /// <summary>
+    /// If the ExitCanvas is open, calling this funktion closes it. 
+    /// If the ExitCanvas is closed, calling this funktion opens it.
+    /// </summary>
+    public void SwitchExitCanvasAvailability()
+    {
+        #if !UNITY_WEBGL
+            bool availability = ExitCanvas.activeSelf;
+            ExitCanvas.SetActive(!availability);
+        #endif
+    }
+
+    /// <summary>
+    /// Exit the Program.
+    /// </summary>
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    /// <summary>
+    /// Start import process.
+    /// </summary>
+    public void InitiateImport()
+    {
+        SaveManager.Import();
+    }
+
+    /// <summary>
+    /// Start export process.
+    /// </summary>
+    public void InitiateExport()
+    {
+        SaveManager.Export();
+    }
+}
