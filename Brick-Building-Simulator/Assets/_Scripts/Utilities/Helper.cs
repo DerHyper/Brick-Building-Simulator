077189a4fb91274f248921c72ed654c98752f865
DerHyper <maxrichter15@web.de>
2024-07-03 21:52:47 +0200

    Refactor Helper

--- Brick-Building-Simulator/Assets/_Scripts/Utilities/Helper.cs
+++ Brick-Building-Simulator/Assets/_Scripts/Utilities/Helper.cs
@@ -2,9 +2,13 @@ using System;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Class with general helper methods.
+/// </summary>
 public static class Helper
 {
-    // Calls the method or every possible position between the start and end position   
+    /// <returns></returns>
+    /// <inheritdoc cref="Helper.DoWithinRange{InType, OutType}(Vector3Int, Vector3Int, Func{Vector3Int, InType, OutType}, InType)"/>  
     public static void DoWithinRange<T>(Vector3Int startPosition, Vector3Int endOffset, Action<Vector3Int> method)
     {
         int xCurrent = 0;
@@ -19,21 +23,22 @@ public static class Helper
                     Vector3Int currentOffset = new Vector3Int(xCurrent, yCurrent, zCurrent);
                     Vector3Int positionWithOffset = startPosition + currentOffset;
                     method(positionWithOffset);
-                    
-                    if (endOffset.z >= 0) zCurrent++; 
+
+                    if (endOffset.z >= 0) zCurrent++;
                     else zCurrent--;
                 }
 
-                if (endOffset.y >= 0) yCurrent++; 
+                if (endOffset.y >= 0) yCurrent++;
                 else yCurrent--;
             }
-            
-            if (endOffset.x >= 0) xCurrent++; 
+
+            if (endOffset.x >= 0) xCurrent++;
             else xCurrent--;
         }
     }
 
-    // DoWithinRange but with an aditional parameter 
+    /// <returns></returns>
+    /// <inheritdoc cref="Helper.DoWithinRange{InType, OutType}(Vector3Int, Vector3Int, Func{Vector3Int, InType, OutType}, InType)"/>
     public static void DoWithinRange<T>(Vector3Int startPosition, Vector3Int endOffset, Action<Vector3Int, T> method, T parameter)
     {
         int xCurrent = 0;
@@ -48,20 +53,20 @@ public static class Helper
                     Vector3Int currentOffset = new Vector3Int(xCurrent, yCurrent, zCurrent);
                     Vector3Int positionWithOffset = startPosition + currentOffset;
                     method(positionWithOffset, parameter);
-                    if (endOffset.z >= 0) zCurrent++; 
+                    if (endOffset.z >= 0) zCurrent++;
                     else zCurrent--;
                 }
 
-                if (endOffset.y >= 0) yCurrent++; 
+                if (endOffset.y >= 0) yCurrent++;
                 else yCurrent--;
             }
-            
-            if (endOffset.x >= 0) xCurrent++; 
+
+            if (endOffset.x >= 0) xCurrent++;
             else xCurrent--;
         }
     }
 
-    // DoWithinRange but with return type
+    /// <inheritdoc cref="Helper.DoWithinRange{InType, OutType}(Vector3Int, Vector3Int, Func{Vector3Int, InType, OutType}, InType)"/>
     public static List<OutType> DoWithinRange<OutType>(Vector3Int startPosition, Vector3Int endOffset, Func<Vector3Int, OutType> method)
     {
         List<OutType> result = new List<OutType>();
@@ -78,23 +83,32 @@ public static class Helper
                     Vector3Int currentOffset = new Vector3Int(xCurrent, yCurrent, zCurrent);
                     Vector3Int positionWithOffset = startPosition + currentOffset;
                     result.Add(method(positionWithOffset));
-                    if (endOffset.z >= 0) zCurrent++; 
+                    if (endOffset.z >= 0) zCurrent++;
                     else zCurrent--;
                 }
 
-                if (endOffset.y >= 0) yCurrent++; 
+                if (endOffset.y >= 0) yCurrent++;
                 else yCurrent--;
             }
-            
-            if (endOffset.x >= 0) xCurrent++; 
+
+            if (endOffset.x >= 0) xCurrent++;
             else xCurrent--;
         }
 
         return result;
     }
 
-    // DoWithinRange but with an aditional parameter and return type
-    public static List<OutType> DoWithinRange<InType,OutType>(Vector3Int startPosition, Vector3Int endOffset, Func<Vector3Int, InType, OutType> method, InType parameter)
+    /// <summary>
+    /// Calls the method or every possible position between the start and end position  
+    /// </summary>
+    /// <typeparam name="InType">The type of the parameter used by the method given as a parameter.</typeparam>
+    /// <typeparam name="OutType">The output type returned by the method given as a parameter.</typeparam>
+    /// <param name="startPosition">Position, which will be used as starting point when iterating.</param>
+    /// <param name="endOffset">Offset, which will be iterated towards to. Can be positive or negative.</param>
+    /// <param name="method">Will be run for every step of the iteration.</param>
+    /// <param name="parameter">Used by the method given as a parameter.</param>
+    /// <returns>List of all the outputs from the given method.</returns>
+    public static List<OutType> DoWithinRange<InType, OutType>(Vector3Int startPosition, Vector3Int endOffset, Func<Vector3Int, InType, OutType> method, InType parameter)
     {
         List<OutType> result = new List<OutType>();
 
@@ -110,15 +124,15 @@ public static class Helper
                     Vector3Int currentOffset = new Vector3Int(xCurrent, yCurrent, zCurrent);
                     Vector3Int positionWithOffset = startPosition + currentOffset;
                     result.Add(method(positionWithOffset, parameter));
-                    if (endOffset.z >= 0) zCurrent++; 
+                    if (endOffset.z >= 0) zCurrent++;
                     else zCurrent--;
                 }
 
-                if (endOffset.y >= 0) yCurrent++; 
+                if (endOffset.y >= 0) yCurrent++;
                 else yCurrent--;
             }
-            
-            if (endOffset.x >= 0) xCurrent++; 
+
+            if (endOffset.x >= 0) xCurrent++;
             else xCurrent--;
         }
 
