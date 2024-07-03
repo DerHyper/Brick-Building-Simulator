a6677b8f67a233b2a612114b5e934c2abb00c135
DerHyper <maxrichter15@web.de>
2024-07-03 19:05:54 +0200

    Refactor SaveManager

--- Brick-Building-Simulator/Assets/_Scripts/Managers/SaveManager.cs
+++ Brick-Building-Simulator/Assets/_Scripts/Managers/SaveManager.cs
@@ -1,39 +1,53 @@
 using UnityEngine;
 using SFB;
 using System.IO;
+using System.Text;
 
 public class SaveManager : MonoBehaviour
 {
     public GridManager GridManager;
     public InventoryManager InventoryManager;
     public BlockReferenceManager BlockReferenceManager;
-
     private const string StdFilePath = ""; // Note: Standard path depending on OS (Windows: C:\Users\<Username>\Documents)
-    // Filter to only show relevant file extentions (.json)
+
+    /// <summary>
+    /// Filter to only show relevant file extentions (.json)
+    /// </summary>
     private readonly ExtensionFilter[] extensions = new ExtensionFilter[] {
-        new ExtensionFilter("Constructions", "json"),
-        new ExtensionFilter("All Files", "*" ),
+        new("Constructions", "json"),
+        new("All Files", "*" ),
     };
 
+    /// <summary>
+    /// Starts an import process:
+    /// Opens a file browser to select a .json file.
+    /// Clears the grid and the inventory.
+    /// Loads the .json file onto the grid.
+    /// </summary>
     public void Import()
-    {   
+    {
         string saveData;
 
         // Depending on the current platform either upload or load the file
-        #if UNITY_WEBGL && !UNITY_EDITOR
-            return; // TODO: Add download for WebGL
-        #else
-            string saveFileLocation = OpenImportFileBrowser();
-            if (saveFileLocation == "") return; // Cancel: No path was selected
+#if UNITY_WEBGL && !UNITY_EDITOR
+        return; // TODO: Add download for WebGL
+#else
+        string saveFileLocation = OpenImportFileBrowser();
+        if (saveFileLocation == "") return; // Cancel: No path was selected
 
-            saveData = ReadJsonToString(saveFileLocation);
-        #endif
+        saveData = ReadJsonToString(saveFileLocation);
+#endif
 
         GridManager.ClearGrid();
         InventoryManager.ClearInventory();
         LoadSaveData(saveData);
     }
 
+    /// <summary>
+    /// Starts an export process:
+    /// Opens a file browser to select a .json file.
+    /// Serializes the blocks on the grid into a JSON-String and saves it. 
+    /// </summary>
     public void Export()
     {
         // Open save panle and get save path
@@ -44,44 +58,48 @@ public class SaveManager : MonoBehaviour
         string saveData = SaveDataToJSON();
 
         // Depending on the current platform either download or write the file
-        #if UNITY_WEBGL && !UNITY_EDITOR
-            return; // TODO: Add download for WebGL
+#if UNITY_WEBGL && !UNITY_EDITOR
+        return; // TODO: Add download for WebGL
 
-        #else
-            File.WriteAllText(saveFileLocation, saveData);
+#else
+        File.WriteAllText(saveFileLocation, saveData);
 
-        #endif
+#endif
 
     }
 
     // Deserialize the JSON file in a format readable to C#
     private string ReadJsonToString(string saveFileLocation)
     {
-        string saveData = "";
+        StringBuilder saveData = new();
 
         // Read each line of the file at saveFileLocation into saveData
-        StreamReader streamReader = new StreamReader(saveFileLocation);
-        while(!streamReader.EndOfStream) saveData += streamReader.ReadLine();
-        streamReader.Close( ); 
+        StreamReader streamReader = new(saveFileLocation);
+        while (!streamReader.EndOfStream)
+        {
+            string line = streamReader.ReadLine();
+            saveData.Append(line);
+        }
+        streamReader.Close();
 
-        return saveData;
+        return saveData.ToString();
     }
 
     private void LoadSaveData(string saveData)
     {
         // Deserialize the saveData
-        JsonData jsonData = JsonUtility.FromJson<JsonData>(saveData); 
-        Debug.Log("Collected Data from file: "+jsonData);
+        JsonData jsonData = JsonUtility.FromJson<JsonData>(saveData);
+        Debug.Log("Collected Data from file: " + jsonData);
 
         // Build all blocks saved in jsonData
-        foreach(JsonData.RelevantBlockData blockInfo in jsonData.JsonDataBlockInfos)
+        foreach (JsonData.RelevantBlockData blockInfo in jsonData.JsonDataBlockInfos)
         {
             // get block information
             Vector3Int position = blockInfo.Position;
             string name = blockInfo.Name;
             Orientation.Alignment alignment = blockInfo.Alignment;
             BuildingBlock blockType = BlockReferenceManager.GetBuildingBlockByName(name);
-            
+
             GridManager.TryInstantiateBuildingBlock(position, blockType, alignment);
         }
     }
@@ -91,10 +109,10 @@ public class SaveManager : MonoBehaviour
         string ImportTitle = "Import your construction";
         bool IsMultiselect = false;
         string[] saveFileLocation = StandaloneFileBrowser.OpenFilePanel(ImportTitle, StdFilePath, extensions, IsMultiselect);
-        
+
         // Catch if no file was selected
-        if(saveFileLocation.Length == 0) return "";
-        else return saveFileLocation[0];
+        string result = (saveFileLocation.Length == 0) ? "" : saveFileLocation[0];
+        return result;
     }
 
     private string OpenExportFileBrowser()
@@ -110,12 +128,12 @@ public class SaveManager : MonoBehaviour
     {
         // Get all Building Blocks
         BuildingBlockDisplay[] blocks = GridManager.GetBlocksInGrid();
-        
+
         // Put the BuildingBlocks in a format that can be serialized into a JSON file
-        JsonData jsonData = new JsonData(blocks);
+        JsonData jsonData = new(blocks);
         string jsonText = JsonUtility.ToJson(jsonData);
-        Debug.Log("Collected Block Data: "+jsonText);
-        
+        Debug.Log("Collected Block Data: " + jsonText);
+
         return jsonText;
     }
 }
