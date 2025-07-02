de82293fb12779f0f3b907ce78d4a96760ae6ad2
DerHyper <maxrichter15@web.de>
2025-07-02 20:29:38 +0200

    Add UI scaling based on device

--- Brick-Building-Simulator/Assets/_Scripts/Managers/MenuManager.cs
+++ Brick-Building-Simulator/Assets/_Scripts/Managers/MenuManager.cs
@@ -1,5 +1,8 @@
+using System;
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 /// <summary>
 /// provides a wrapper for access to and from menus. He takes care of opening and closing panels within the software.
@@ -11,9 +14,17 @@ public class MenuManager : MonoBehaviour
     public GameObject ExportCanvas;
     public GameObject OptionsCanvas;
     public GameObject ExitButton;
+    public GameObject MainCanvas;
     public SaveManager SaveManager;
+
+    /// <summary>
+    /// Changes the reference resolution by its factor.
+    /// Default is 1, higher number results in smaller UI
+    /// </summary>
+    public float StandaloneReferenceResolutionFactor = 1;
     void Start()
     {
+        InitiateMenuScale();
         InitiateExitMenu();
         ImportCanvas.SetActive(false);
         ExportCanvas.SetActive(false);
@@ -129,5 +140,22 @@ public class MenuManager : MonoBehaviour
         ExitButton.SetActive(false); // No exit on WebGL, Mobile
 #endif
         ExitCanvas.SetActive(false);
+    }
+
+    /// <summary>
+    /// Change menu scales based on device.
+    /// </summary>
+    private void InitiateMenuScale()
+    {
+        HashSet<GameObject> canvases = new() { ExitCanvas, ImportCanvas, ExportCanvas, OptionsCanvas, MainCanvas};
+
+        foreach (var canvas in canvases)
+        {
+            Vector2 currentResolution = canvas.GetComponent<CanvasScaler>().referenceResolution;
+
+#if (!UNITY_IOS && !UNITY_ANDROID) || UNITY_EDITOR // Not Mobile
+            canvas.GetComponent<CanvasScaler>().referenceResolution = currentResolution * StandaloneReferenceResolutionFactor;
+#endif
+        }
     }
 }
